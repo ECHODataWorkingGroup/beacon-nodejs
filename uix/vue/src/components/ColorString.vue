328d0ffceeb0e4bf246d8d5e93204fa349bdcb78
Joe Kane <jkane@fsjd.org>
2024-05-22 17:17:42 +0200

    adds new vue component;
    ColorString:
      props:
       colArr: Array
       tehString: String

---
+++ uix/vue/src/components/ColorString.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+  import { ref, defineProps } from 'vue'
+  const props = defineProps({
+    colArr: Array,
+    tehString: String
+  })
+
+  console.log(props)
+  const applyCssToString = function(){
+
+    const tehString = props.tehString
+    const colArr = props.colArr
+    const numColors = colArr.length 
+   
+    var retHtml = "" 
+    var j = 0
+    for( var i = 0; i < tehString.length; i++ ){ 
+      retHtml += `<span style="color: ${colArr[j++ % numColors]};">${tehString.charAt(i)}</span>`
+    } 
+  
+    return retHtml  
+  }
+
+</script>
+
+<template>
+  <span v-html="applyCssToString()"></span>
+</template>
