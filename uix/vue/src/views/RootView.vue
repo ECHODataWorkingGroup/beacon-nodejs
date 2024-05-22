9aabcdb17f67274875755537c7f1dcac4eb16cee
Joe Kane <jkane@fsjd.org>
2024-05-22 17:24:58 +0200

    Colori[sz]e the string "ECHO"

--- uix/vue/src/views/RootView.vue
+++ uix/vue/src/views/RootView.vue
@@ -25,7 +25,7 @@
 <template>
   <div class="center">
     <h3>Hello! Intrepid BioInfo-type person!</h3>
-    <h2>This is the Dept-Inst Beacon (v2) Landing Page.</h2>
+    <h2>This is the</h2><colstr :colArr="colArr" tehString="ECHO"/><h2> (v2) Landing Page.</h2>
   </div>
 <p class="center">It's probably the case you want to be calling this API via programatic means, 
     rather than browsing it on the web...try: curl -XPOST ...</p>
