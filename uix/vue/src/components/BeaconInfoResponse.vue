feb40373399d48f6aaa7d8b4c43280705fa91c2c
Joe Kane <jkane@fsjd.org>
2024-05-23 18:35:42 +0200

    add a debug flag; used to hide json editor
    just to keep things a bit cleaner, whilst retaining functionality

--- uix/vue/src/components/BeaconInfoResponse.vue
+++ uix/vue/src/components/BeaconInfoResponse.vue
@@ -3,10 +3,17 @@
   import JsonEditor from 'json-editor-vue'
 	// import { beaconInfoResponseSchema } from '../../../../schema/mongoose/beacon/framework/responses/beaconInfoResponse.js'
   // eslint-disable-next-line
-  import { ref, inject } from 'vue'
+  import { ref, inject, defineProps } from 'vue'
   import { schemaToProps } from '../composables/schema/mongoose/utils.js'
 
   const apiClient = inject('apiClient')
+
+  const props = defineProps({
+    debug: {
+      type: Boolean,
+      default: false
+    }
+  })
 
   const beaconInfoResponse = ref("Fetching The Info Now...(that's a smashing blouse, btw!)")
   beaconInfoResponse.value  = await apiClient.fetch( '/info', {}, { auth: 'basic' } ).then(
@@ -25,7 +32,7 @@
 
 <template>
 <div class="horiz">
-<div>
+<div v-if="debug">
   <JsonEditor class="jse-theme-dark" v-model="beaconInfoResponse" />
 </div>
 <div>
